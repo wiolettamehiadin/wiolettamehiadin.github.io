513b56807908a0d3fe79d6707ad4915b953a2ea1
wiolettamehiadin <wioletta_k@live.co.uk>
2021-06-21 21:59:37 +0100

    Rozwiazanie warsztatu Accordion jQuery

---
+++ 4-accordion-jquery/js/main.js
@@ -0,0 +1,28 @@
+
+//1. podpiecie jQuery
+//2. zdarzenie click
+//3. rozwiniecie paragrafu // this -element klikniety
+
+$(document).ready(function(){
+
+    $('.accordion-item').click(function() {
+
+    if($(this).hasClass('open')) {   //jezeli ma klase open - slide up
+        $(this).removeClass('open');
+        $(this).find('.accordion-content').slideUp();
+
+    } else {
+        $(this).addClass('open');   
+        $(this).find('.accordion-content').slideDown();  //jezeli nie ma klasy open- slide down
+    }
+
+
+
+
+  });
+
+});
+
+
+
+
